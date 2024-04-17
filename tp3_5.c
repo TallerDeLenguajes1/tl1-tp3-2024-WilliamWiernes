9c3c6ebf1791678a9d4ceb6b4a9f7be869c7b6c0
“WilliamWiernes” <williambrandonwiernes@gmail.com>
2024-04-17 17:46:25 -0300

    Ejercicio 5

--- tp3_5.c
+++ tp3_5.c
@@ -11,18 +11,18 @@ int main()
     // Variable auxiliar
     char *buff = (char *)malloc(100 * sizeof(char));
 
-    // Creo arreglo forma dinámica
+    // Creo el arreglo de forma dinámica
     int cantNombres;
     printf("Ingrese la cantidad de nombres: ");
     scanf("%d", &cantNombres);
 
-    char **nombres = (char **)malloc(cantNombres * sizeof(char));
+    char **nombres = (char **)malloc(cantNombres * sizeof(char *));
 
-    // Cargo la cantidad de nombres
+    // Cargo la cantidad de cantNombres nombres
     for (int i = 0; i < cantNombres; i++)
     {
         printf("Ingrese el nombre %d: ", i + 1);
-        gets(buff);
+        scanf("%s", buff);
         *(nombres + i) = (char *)malloc((strlen(buff) + 1) * sizeof(char));
         strcpy(*(nombres + i), buff);
     }
@@ -30,17 +30,19 @@ int main()
     // Muestro por pantalla los nombres
     for (int i = 0; i < cantNombres; i++)
     {
-        printf("Nombre %d: ", i + 1);
+        printf("Nombre %d:", i + 1);
         puts(*(nombres + i));
     }
 
     // Libero memoria asignada dinámicamente
     free(buff);
-    free(nombres);
+
     for (int i = 0; i < cantNombres; i++)
     {
-        free(nombres + i);
+        free(*(nombres + i)); // Libero cada puntero dentro de nombres
     }
+
+    free(nombres); // Y aquí libero nombres como tal
 
     return 0;
 }
